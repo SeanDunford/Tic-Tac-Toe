3161b9cd1222cbb00f0fb8702c55e7581590a985
sean dunford <sdunford@mail.usf.edu>
2012-07-08 16:33:29 -0400

    inital update

--- App_Code/Tic_Tac_Toe_Service.cs
+++ App_Code/Tic_Tac_Toe_Service.cs
@@ -5,13 +5,14 @@ using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.Xml.Linq;
 
-[WebService(Namespace = "http://tempuri.org/")]
+[WebService(Namespace = "http://cis4930wp.eng.usf.edu/wpusr07")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
 // [System.Web.Script.Services.ScriptService]
-public class Service : System.Web.Services.WebService
+public class Tic_Tac_Toe_Service : System.Web.Services.WebService
 {
-    public Service () {
+    public string test; //needs persistance
+    public Tic_Tac_Toe_Service () {
 
         //Uncomment the following line if using designed components 
         //InitializeComponent(); 
--- Tic_Tac_Toe_Service.asmx
+++ Tic_Tac_Toe_Service.asmx
@@ -1,1 +1,1 @@
-﻿<%@ WebService Language="C#" CodeBehind="~/App_Code/Service.cs" Class="Service" %>
+﻿<%@ WebService Language="C#" CodeBehind="~/App_Code/Service.cs" Class="Tic_Tac_Toe_Service" %>
